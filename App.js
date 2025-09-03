05decb740e46883541e4076834e78b7333a5ef8e
helpbringer6-ui <bdtwalalo@hotmail.com>
2025-09-03 12:14:14 -0500

    Update title for localization

--- App.js
+++ App.js
@@ -5,7 +5,7 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <h1>Virtueller Verkaufsassistent</h1>
+      <h1>Virtual Sales Assistant</h1>
       <ProductList />
     </div>
   );
