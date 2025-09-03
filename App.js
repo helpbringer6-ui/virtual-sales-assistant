495335de10c85c7c211d2b04b7d2d0abd1666c8e
helpbringer6-ui <bdtwalalo@hotmail.com>
2025-09-03 12:15:02 -0500

    Update title to 'Virtual Sales Assistant - Your One-Stop Shop'

--- App.js
+++ App.js
@@ -5,7 +5,7 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <h1>Virtual Sales Assistant</h1>
+      <h1>Virtual Sales Assistant - Your One-Stop Shop</h1>
       <ProductList />
     </div>
   );
